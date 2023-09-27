bacef87119360195259bb6faefcf81b5a03df48c
NDenheim <nicole.denheim@hotmail.co.uk>
2023-09-27 15:58:35 +0100

    Removed unnecessary code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,30 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-//        Snap snap = new Snap();
-//        snap.getDeck();
-//        System.out.println("\nTop card:\n");
-//        snap.dealCard();
-////        System.out.println("Sorted in number order");
-////        System.out.println("Sorted in suit order");
-//        snap.shuffleDeck();
-//        snap.getDeck();
-//        System.out.println("\nTest number:\n");
-//        snap.sortDeckInNumberOrder();
-//        System.out.println("\nTest suit:\n");
-//        snap.sortDeckIntoSuits();
-
-//        snap.getDeck();
-//        snap.dealCard();
-//        snap.dealCard();
-//        System.out.println("---------------");
-//        snap.getDeck();
-//        System.out.println("---------------");
-//        snap.getDeck();
-
-
-//        snap.runGame();
-
         Commands.runCommands();
 
     }
